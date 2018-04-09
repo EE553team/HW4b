e09f9a5f4f6dc3ce72fced0077fee5baa98ecc3e
Hanzheng Qin <35711199+qinhanzeng@users.noreply.github.com>
2018-04-09 16:55:55 -0400

    The one file version of Prinscript

--- onefileversion.cpp
+++ onefileversion.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<fstream>
-#include"Postscript.h"
 using namespace std;
 class Postscript {
 private:
@@ -48,9 +47,67 @@ public:
     }
     void text(double x,double y,char a[100]) {
         fstream fs;
-        fs.open(name,ios::app);
-        fs<<"/Courier 20 selectfont "<<x<<' '<<y<<" moveto ("<<a<<") show"<<endl;
+        fs.open(name, ios::app);
+        fs << "/Courier 20 selectfont " << x << ' ' << y << " moveto (" << a << ") show" << endl;
         fs.close();
+    }
+    void setFillColor(int x, int y, int z){
+        rf=x;
+        gf=y;
+        bf=z;
+
+    }
+
+    void setStrokeColor(int x, int y, int z){
+        rs=x;
+        gs=y;
+        bs=z;
+    }
+
+
+    void drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3){
+        ofstream fs;
+        fs.open(name,ios::app);
+        fs<<x1<<' '<<y1<<' '<<"moveto"<<'\n'<<x2<<' '<<y2
+          <<' '<<"lineto"<<'\n'<<x3<<' '<<y3<<' '<<"lineto"<<'\n'
+          <<x1<<' '<<y1<<' '<<"lineto"<< '\n'<<"closepath"<<
+          '\n'<<rs<<' '<<gs<<' '<<bs<<' '<< "setrgbcolor"<<'\n'<<
+          "4 setlinewidth"<<'\n'<<"stroke"<<'\n';
+        fs.close();
+    }
+
+    void fillTriangle(int x1, int y1, int x2, int y2, int x3, int y3){
+        ofstream fs;
+        fs.open(name,ios::app);
+        fs<<x1<<' '<<y1<<' '<<"moveto"<<'\n'<<x2<<' '<<y2
+          <<' '<<"lineto"<<'\n'<<x3<<' '<<y3<<' '<<"lineto"<<'\n'
+          <<x1<<' '<<y1<<' '<<"lineto"<< '\n'<<"closepath"<<
+          '\n'<<rf<<' '<<gf<<' '<<bf<<" setrgbcolor"<<'\n'<<"fill"<<'\n';
+        fs.close();
+    }
+
+    void drawCircle(int x,int y,int radius){
+        ofstream fs;
+        fs.open(name,ios::app);
+        fs<<x<<' '<<y<<' '<<radius<<' '<<0<<' '<<360<<' '<<"arc"<< '\n' <<
+          rs<<' '<<gs<<' '<<bs<<' '<< "setrgbcolor"<<'\n'<<
+          "4 setlinewidth"<<'\n'<<"stroke"<<'\n';
+        fs.close();
+    }
+
+
+    void grid(int x1, int interval_x, int x2, int y1, int interval_y, int y2){
+        ofstream fs;
+        fs.open(name,ios::app);
+        int bins_x = ((x2 - x1) / interval_x);
+        int bins_y = ((y2 - y1) / interval_y);
+        fs<<"/vline"<<' '<<"{"<<x1<<' '<<y1<<' '<<"moveto"<<' '<<x1<<' '<<y2<<' '<<"lineto"<<' '<<"stroke}"<<' '<<"def"<<'\n'<<
+          "gsave"<< '\n'<<"vline"<<'\n'<<bins_x<<' '<<"{"<<interval_x<<' '<<0<<' '<<"translate vline}"<<' '<<"repeat"<<'\n'<<"grestore"<<'\n'<<
+          "/hline"<<' '<<"{"<<x1<<' '<<y1<<' '<<"moveto"<<' '<<x2<<' '<<y1<<' '<<"lineto stroke}"<<' '<<"def"<<'\n'<<
+          "gsave"<<'\n'<<"hline"<<'\n'<<bins_y<<' '<<"{0"<<' '<<interval_y<<' '<<"translate hline}"<<' '<<"repeat"<<'\n'<<
+          "grestore"<<'\n';
+        fs.close();
+    }
 };
 int main() {
     Postscript p("test.ps");
